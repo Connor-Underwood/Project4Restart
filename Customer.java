d3832b287872449f160f7aaf029026d31f7d65ef
Connor Underwood <ctu266667@gmail.com>
2022-11-18 22:34:58 -0500

    viewMarket() method

--- Customer.java
+++ Customer.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.io.*;
 /**
  * Connor Underwood, Zeyad Adham, Suhani Yadav, Neel Acharya
  *
@@ -63,7 +67,43 @@ public class Customer {
      * @param searchString If search is true, set searchString to their input. Otherwise, set searchString to "" empty string
      */
     public void viewMarket(boolean search, String searchString) {
+        if (search) {
 
+        } else {
+            ArrayList<String> market = new ArrayList<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader("market.csv"))) {
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    String[] arr = line.split(",");
+                    if (arr.length > 4) {
+                        String email = arr[1].substring(0,arr[1].indexOf("@"));
+                        String store = arr[3];
+                        line = "Seller: " + email + "\nStore: " + store + "\n";
+                        for (int i = 4; i < arr.length; i+=4) {
+                            line += "- " + arr[i] + ",$" + arr[i+2] + "\n";
+                        }
+                        market.add(line);
+                    }
+
+                }
+            } catch (IOException io) {
+                System.out.println("Error reading to the market.csv file.");
+            }
+            File f = new File(this.email + ".txt");
+            try {
+                boolean b = f.createNewFile();
+            } catch (IOException io) {
+                System.out.println("Error creating new file.");
+            }
+            try (PrintWriter writer = new PrintWriter(new FileWriter(f))) {
+                for (int i = 0; i < market.size(); i++) {
+                    writer.println(market.get(i));
+                    System.out.println(market.get(i));
+                }
+            } catch (IOException io) {
+                System.out.println("Error writing to file.");
+            }
+        }
     }
 
 
